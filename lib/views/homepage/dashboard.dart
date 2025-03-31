6d116c7ed81e0b64045ff5503ba432b75b70006c
Hamza Rafique <103266161+hamzeedev@users.noreply.github.com>
2025-03-31 17:47:22 +0500

    Create dashboard.dart

---
+++ lib/views/homepage/dashboard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
